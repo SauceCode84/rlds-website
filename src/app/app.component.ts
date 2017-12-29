bc8eb4ce7909c9c5289aeccf0988f9e1af5256e6
SauceCode84 <rayruok@gmail.com>
2017-12-29 12:01:59 +0200

    added footer added Font Awesome 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from "@angular/core";
+import { Meta } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent { }
+export class AppComponent {
+
+  constructor(private meta: Meta) {
+    this.meta.addTag({
+      name: "description",
+      content: ""
+    });
+  }
+
+}
